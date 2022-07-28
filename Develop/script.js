a03f033d5ae8b99b57752974697a1b6432ea94c2
RodBennett <rod.bennett75@gmail.com>
2022-07-28 16:21:22 -0600

    final push

--- Develop/script.js
+++ Develop/script.js
@@ -1,57 +1,39 @@
 // Global variables
-const timesClasses = ['9', '10', '11', '12', '13', '14', '15', '16', '17']
-const saveButtonEl = $('.saveBtn')
-let checkTime = parseInt(moment().format('H'))
-const textArea = $('.textarea')
-let dataEntries = [ '#A', '#B', '#C', '#D', '#E', '#F', '#G', '#H', '#I']
-let toDos = ['', '', '', '', '', '', '', '', '', '']
+// let presentTime = parseInt(moment().format('H'))
+// let hourCheck = time.indexOf(presentTime)
+// const textArea = $('.textarea')
+// let dataEntries = [ '#A', '#B', '#C', '#D', '#E', '#F', '#G', '#H', '#I']
+let toDos = ['']
 
 // // current date and time
 var today = moment().format('[Today is ] MMM Do, YYYY');
 $("#currentDay").text(today)
 
-// setting up different buttons to accept respective textareas
-$(document).ready(function() {
-    $('.saveBtn').click(function() {
-        $(this).siblings('.textarea').val()
-        saveEntries()
-    })
+// setting up different buttons to accept respective textareas and set to local storage
+$(document).ready(function () {
+$(".saveBtn").on("click", function () {
+    var save = $(this).siblings('.textarea').val();
+    var saveBtns =  $(this).parent().attr('id')
+    localStorage.setItem(saveBtns, save)
 })
 
-function saveEntries() {
-    var saved = textArea.val()
-    localStorage.setItem.json.stringify('saved', )
-}
+// code to retrieve To Do entries (setItem / getItem)
+
+    $('#9 .textarea').val(localStorage.getItem('9'))
+    $('#10 .textarea').val(localStorage.getItem('10'))
+    $('#11 .textarea').val(localStorage.getItem('11'))
+    $('#12 .textarea').val(localStorage.getItem('12'))
+    $('#13 .textarea').val(localStorage.getItem('13'))
+    $('#14 .textarea').val(localStorage.getItem('14'))
+    $('#15 .textarea').val(localStorage.getItem('15'))
+    $('#16 .textarea').val(localStorage.getItem('16'))
+    $('#17 .textarea').val(localStorage.getItem('17'))
+})
+    // code to clear data from To Do rows
+$('.clear-button').on('click', function() {
+    localStorage.clear()
+})
 
 // code to change color scheme according to the day
 
-// if (checkTime > 17) {
-//     for (let index = 0; index <timesClasses.length; index++) {
-//          $(`${timesClasses[i]}`)
-//     $('.textarea').addClass('past')
-//     }
-// } else if (checkTime < 9) {
-//     $(`${timesClasses[i]}`)
-//     $('.textarea').addCLass('future')
-//     console.log(timesClasses)
-// }
-// } else {
-//     checkThisHour()
-// }
 
-// function checkThisHour() {
-// let thisTime = timeClasses.entry
-// for (let index = 0; index < timeClasses.length; index++) {
-//     if (checkTime == thisTime) {
-//             $('.textarea').addClass('present')
-//     }
-//    }
-//  }
-
-// code to store save and retirve To Do entries (setItem / getItem)
-
-// allBtns.addEventListener('click', function() {
-//     var 
-    
-//     localStorage.setItem('A', 'value')
-// })
